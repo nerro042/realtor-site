d59e01c62616b86c61a0989337a278f5ab4a74f3
Kingsley <ekehkingsley@yahoo.com>
2023-09-02 10:43:28 +0100

    completed the sign-in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function SignIn() {
   const [showPassword, setShowPassword] = useState(false);
@@ -9,6 +11,7 @@ export default function SignIn() {
     email: "",
     password: "",
   });
+  const navigate = useNavigate();
 
   const { email, password } = formData;
 
@@ -19,6 +22,25 @@ export default function SignIn() {
       ...prevState,
       [e.target.id]: e.target.value,
     }));
+  };
+
+  const onsubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error("wrong user credentials");
+    }
   };
 
   const togglePasswordVisibility = () => {
@@ -38,7 +60,7 @@ export default function SignIn() {
         </div>
 
         <div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-          <form>
+          <form onSubmit={onsubmit}>
             <input
               type="email"
               id="email"
